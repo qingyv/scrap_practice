38f1da0013e82f1c68f996beaa74a2c791dd81c8
qingyv <qingyv142857@gmail.com>
2019-03-26 12:16:05 +0800

    one day!

--- scrapy.py
+++ scrapy.py
@@ -1,5 +1,15 @@
-print("this is my first practice for scrapy!")
-print('hello world')
-print("it's time to study jcod!")
-print('the greatest thing in life aren\'t thing!')
-print("just do it no matter what happen!")
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://list.tmall.com/search_product.htm?spm=a222t.7794920.fsnav.8.6fb839edjCElIh&cat=50924003&acm=lb-zebra-24139-328537.1003.4.455785&scm=1003.4.lb-zebra-24139-328537.OTHER_14458839888552_455785'
+html = requests.get(url).content
+soup = BeautifulSoup(html, 'lxml')
+for product_list in soup.findAll('div', 'product'):
+    product_title = product_list.find('p', 'productTitle').a.text
+    print(product_title)
+
+    product_price = product_list.find('p', 'productPrice').em.text
+    print(product_price)
+
+    product_status = product_list.find('p', 'productStatus').text
+    print(product_status)
